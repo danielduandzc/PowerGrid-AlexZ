ff91ae23396aab28f289edbbe50d02f8e4b1eeb4
DongXinyueK1907354 <k1907354@students.katyisd.org>
2026-02-20 07:56:24 -0600

    Update PowerGrid.java

--- PowerGrid.java
+++ PowerGrid.java
@@ -1,1 +1,13 @@
-
+import java.awt.*;
+import javax.swing.*;
+public class PowerGrid extends JFrame {
+	private final int WIDTH = 1200;
+	private final int HEIGHT = 1200;
+	public PowerGrid(String frameName) {
+		super(frameName);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		add(new PowerGridPanel());
+		setVisible(true);
+	}
+}
